227c5f463b5c4f8c01dcce8ca367e99b0e62aca2
Coolbassist <sparta8oy@gmail.com>
2018-09-02 18:48:53 +0100

    Changed while loop conditions

--- main.cpp
+++ main.cpp
@@ -11,29 +11,25 @@ int main()
     int number_of_primes_to_look_for = 100;
     int number_of_primes_found = 0;
     int primes_found[number_of_primes_to_look_for];
-    while(1)
+    while(number_of_primes_found != number_of_primes_to_look_for)
     {
         if(prime_finder(current_number))
         {
-            //std::cout << current_number << " is a prime number" << std::endl;
             primes_found[number_of_primes_found] = current_number;
             number_of_primes_found += 1;
         }
         current_number+=2;
-
-        if(number_of_primes_found == number_of_primes_to_look_for)
-        {
-            int stop_s=clock();
-            std::cout << "The first " << number_of_primes_to_look_for << " primes are: ";
-            for(int i = 0; i < number_of_primes_to_look_for-1; i++)
-            {
-                std::cout << primes_found[i] << ", ";
-            }
-            std::cout << primes_found[number_of_primes_to_look_for-1] << ".";
-            std::cout << "\nTime elapsed: " << (stop_s-start_s)/double(CLOCKS_PER_SEC)*1000 << std::endl;
-            return 0;
-        }
     }
+    int stop_s=clock();
+
+    //now to display the data
+    std::cout << "The first " << number_of_primes_to_look_for << " primes are: ";
+    for(int i = 0; i < number_of_primes_to_look_for-1; i++)
+    {
+        std::cout << primes_found[i] << ", ";
+    }
+    std::cout << primes_found[number_of_primes_to_look_for-1] << ".";
+    std::cout << "\nTime elapsed: " << (stop_s-start_s)/double(CLOCKS_PER_SEC)*1000 << std::endl;
     return 0;
 }
 
