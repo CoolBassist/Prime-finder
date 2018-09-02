36f75d356742529798f7e454e64ee7164999efbf
Coolbassist <sparta8oy@gmail.com>
2018-09-02 18:16:28 +0100

    Made considerably faster

--- main.cpp
+++ main.cpp
@@ -7,28 +7,37 @@ int prime_finder(int number);
 int main()
 {
     int start_s=clock();
-    int current_number = 0;
+    int current_number = 1;
+    int number_of_primes_to_look_for = 100;
     int number_of_primes_found = 0;
+    int primes_found[number_of_primes_to_look_for];
     while(1)
     {
-        if(primer_finde(current_number))
+        if(prime_finder(current_number))
         {
-            std::cout << current_number << " is a prime number" << std::endl;
+            //std::cout << current_number << " is a prime number" << std::endl;
+            primes_found[number_of_primes_found] = current_number;
             number_of_primes_found += 1;
         }
-        current_number++;
+        current_number+=2;
 
-        if(number_of_primes_found == 1000000)
+        if(number_of_primes_found == number_of_primes_to_look_for)
         {
             int stop_s=clock();
-            std::cout << "time: " << (stop_s-start_s)/double(CLOCKS_PER_SEC)*1000 << std::endl;
+            std::cout << "The first " << number_of_primes_to_look_for << " primes are: ";
+            for(int i = 0; i < number_of_primes_to_look_for-1; i++)
+            {
+                std::cout << primes_found[i] << ", ";
+            }
+            std::cout << primes_found[number_of_primes_to_look_for-1] << ".";
+            std::cout << "\nTime elapsed: " << (stop_s-start_s)/double(CLOCKS_PER_SEC)*1000 << std::endl;
             return 0;
         }
     }
     return 0;
 }
 
-int primer_finde(int number)
+int prime_finder(int number)
 {
     for(int i = 2; i <= sqrt(number); i++)
     {
