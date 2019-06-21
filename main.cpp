7bcc055d8765d33bd2515e96d2027460300856c8
CoolBassist <sparta8oy@gmail.com>
2019-06-21 01:06:10 +0100

    Improved speed
    prime_finder now only calculates the squareroot once. Now runs 20x faster.

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
-#include <math.h>
+#include <vector>
+#include <cmath>
 #include <ctime>
 
 int prime_finder(int number);
@@ -10,36 +11,34 @@ int main()
     int current_number = 1;
     int const number_of_primes_to_look_for = 1000000;
     int number_of_primes_found = 0;
-    int primes_found[number_of_primes_to_look_for];
+    std::vector<int> primes_found;
     while(number_of_primes_found != number_of_primes_to_look_for)
     {
         if(prime_finder(current_number))
         {
-            primes_found[number_of_primes_found] = current_number;
+            primes_found.push_back(current_number);
             number_of_primes_found += 1;
         }
         current_number+=2;
     }
     int stop_s=clock();
-            std::cout << "The first " << number_of_primes_to_look_for << " primes are: ";
-            for(int i = 0; i < number_of_primes_to_look_for-1; i++)
-            {
-                std::cout << primes_found[i] << ", ";
-            }
-            std::cout << primes_found[number_of_primes_to_look_for-1] << ".";
-            std::cout << "\nTime elapsed: " << (stop_s-start_s)/double(CLOCKS_PER_SEC)*1000 << std::endl;
-            return 0;
+    std::cout << "The first " << number_of_primes_to_look_for << " primes are: ";
+    for(int i = 0; i < number_of_primes_to_look_for-1; i++) std::cout << primes_found[i] << ", ";
+    std::cout << primes_found[number_of_primes_to_look_for-1] << ".";
+    std::cout << "\nTime elapsed: " << (stop_s-start_s)/double(CLOCKS_PER_SEC)*1000 << std::endl;
+    return 0;
 }
 
 int prime_finder(int number)
 {
+    int sq_rt = sqrt(number);
+
     if(number % 2 == 0)
         return 0;
     
-    for(int i = 3; i <= sqrt(number); i+=2)
-    {
+    for(int i = 3; i <= sq_rt; i+=2)
         if(number % i == 0)
             return 0;
-    }
+
     return 1;
 }
